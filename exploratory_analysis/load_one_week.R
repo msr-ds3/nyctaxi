ab022a90b83fa2fc016cf721a5fa52e48a285c56
Abraham Neuwirth <abeneuwirth@gmail.com>
2016-07-13 13:42:26 -0400

    added comments, fixed typo

--- exploratory_analysis/load_one_week.R
+++ exploratory_analysis/load_one_week.R
@@ -8,9 +8,12 @@ parse_datetime <- function(s, format="%Y-%m-%d %H:%M:%S") {
 }
 
 #FILE DEPENDENCIES - TRIP_FARE_7 AND TRIP_DATA_7 CSVS
+
+# range of dates we're interested in
 range_begin <- as.Date("2013-07-07")
 range_end <- as.Date("2013-07-13")
 
+# read the files, add a ymd col and get the dates in ou range
 trip_fare <- read_csv("trip_fare_7.csv")
 trip_fare <- trip_fare %>% mutate( ymd_pickup = as.Date(parse_datetime(pickup_datetime, "%Y-%m-%d"))) %>% 
                            filter(as.Date(ymd_pickup) >= range_begin & as.Date(ymd_pickup) <= range_end)
@@ -19,8 +22,9 @@ trip_data <- read_csv("trip_data_7.csv")
 trip_data <- trip_data %>% mutate( ymd_pickup = as.Date(parse_datetime(pickup_datetime, "%Y-%m-%d"))) %>% 
                            filter(as.Date(ymd_pickup) >= range_begin & as.Date(ymd_pickup) <= range_end)
 
+# join the two dataframes and save to Rdata file
 taxi <- inner_join(trip_data, trip_fare, by=c("medallion", "hack_license", "vendor_id", "pickup_datetime", "ymd_pickup"))
-taxi <- taxi %>% mutate(hour = hour(pickup_datetime),  days_of_the_week = wday(ymd_pickup,label = TRUE)) 
+taxi <- taxi %>% mutate(hour = hour(pickup_datetime),  day_of_the_week = wday(ymd_pickup,label = TRUE)) 
 
 save(taxi, file = 'one_week_taxi.Rdata')
 
