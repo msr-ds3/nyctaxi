d6ed61d5807613aa2083823399d0b1e06008736d
Fatima Chebchoub <Fatimachebchoub11@gmail.com>
2016-07-18 17:58:46 -0400

    added scale_colour_gradientn to the function

--- exploratory_analysis/visualize_trips.R
+++ exploratory_analysis/visualize_trips.R
@@ -3,7 +3,9 @@ library(maptools)
 library(scales)
 library(tidyr)
 
-sample_driver_trips <- taxi_clean %>% filter(hack_license == "938EF5D4604A7AB7F6C113C9C26FEAEE", day_of_the_week == "Mon") %>% mutate(index = 1:nrow(.))
+sample_license <- sample(unique(taxi_clean$hack_license),1)
+
+sample_driver_trips <- taxi_clean %>% filter(hack_license == sample_license, day_of_the_week == "Sat") %>% arrange (pickup_datetime) %>% mutate(index = 1:nrow(.))
 
 #reshape data so that each observation has a dropoff/pickup row with the corresponding latitude and longitude (used to ease use of geom_path)
 sample_driver_trips = sample_driver_trips %>%
@@ -17,12 +19,14 @@ sample_driver_trips = sample_driver_trips %>%
 
 visualize_trips <- function(df, longitude, latitude, stop_type, index)
 {
-  nyc_map <- get_map(location = c(lon = -73.968227, lat = 40.777127), maptype = "terrain", zoom = 13, color="bw")
+  nyc_map <- get_map(location = c(lon = -73.968227, lat = 40.777127), maptype = "terrain", zoom = 12, color="bw")
   
-  ggmap(nyc_map) +  geom_point(data = df, mapping = aes(x=longitude, y=latitude, shape=stop_type), size=3)+  
-    geom_path(data = df, mapping = aes(x=longitude, y=latitude, color=as.factor(index))) 
+  ggmap(nyc_map) +  geom_point(data = df, mapping = aes(x=longitude, y=latitude, shape=stop_type, color= index), size=3)+  
+    geom_path(data = df, mapping = aes(x=longitude, y=latitude, color=index )) + scale_colour_gradientn(colours=rainbow(3))
 }
 
-
 ### Calling the function:
-visualize_trips(sample_driver_trips, sample_driver_trips$longitude, sample_driver_trips$latitude,sample_driver_trips$stop_type, sample_driver_trips@index)
+visualize_trips(sample_driver_trips, sample_driver_trips$longitude, sample_driver_trips$latitude,sample_driver_trips$stop_type, sample_driver_trips$index)
+
+
+
