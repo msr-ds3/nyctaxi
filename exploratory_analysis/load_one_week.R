03d25229b03ee9734f826fde87545f6e5d0426af
Jai Punjwani <jaipunjwani@mail.adelphi.edu>
2016-07-18 16:08:52 -0400

    moved taxi_clean df to load_one_week

--- exploratory_analysis/load_one_week.R
+++ exploratory_analysis/load_one_week.R
@@ -1,7 +1,10 @@
 library(readr)
 library(dplyr)
-library(broom)
 library(lubridate)
+library(tidyr)
+library(httr)
+library(rgdal)
+library(broom)
 
 parse_datetime <- function(s, format="%Y-%m-%d %H:%M:%S") {
   as.POSIXct(as.character(s), format=format)
@@ -26,12 +29,55 @@ trip_data <- read_csv("trip_data_7.csv")
 trip_data <- trip_data %>% mutate( ymd_pickup = as.Date(parse_datetime(pickup_datetime, "%Y-%m-%d"))) %>% 
                            filter(as.Date(ymd_pickup) >= range_begin & as.Date(ymd_pickup) <= range_end)
 
-# join the two dataframes and save to Rdata file
+# join the two dataframes 
 taxi <- inner_join(trip_data, trip_fare, by=c("medallion", "hack_license", "vendor_id", "pickup_datetime", "ymd_pickup"))
 taxi <- taxi %>% mutate(pickup_hour = hour(pickup_datetime), dropoff_hour = hour(dropoff_datetime),day_of_the_week = wday(ymd_pickup,label = TRUE), 
                         pickup_minute = parse_minute(pickup_datetime), dropoff_minute = parse_minute(dropoff_datetime)) 
 
-save(taxi, file = 'one_week_taxi.Rdata')
+get_nyc_neighborhoods <- function(){
+  r <- GET('http://data.beta.nyc//dataset/0ff93d2d-90ba-457c-9f7e-39e47bf2ac5f/resource/35dd04fb-81b3-479b-a074-a27a37888ce7/download/d085e2f8d0b54d4590b1e7d1f35594c1pediacitiesnycneighborhoods.geojson')
+  return(readOGR(content(r,'text'), 'OGRGeoJSON', verbose = F))
+}
+
+# get nyc boundaries
+nyc_neighborhoods <- get_nyc_neighborhoods()
+
+nyc_df <- tidy(nyc_neighborhoods)
+min_lat <- min(nyc_df$lat)
+max_lat <- max(nyc_df$lat)
+min_lng <- min(nyc_df$long)
+max_lng <- max(nyc_df$long)
+
+# filter out pickups and dropoffs outside of nyc boundaries
+taxi_clean <- filter(taxi,  pickup_longitude >= min_lng & pickup_longitude <= max_lat & pickup_latitude >= min_lat & pickup_latitude <= max_lat &
+                       dropoff_longitude >= min_lng & dropoff_longitude <= max_lat & dropoff_latitude >= min_lat & dropoff_latitude <= max_lat)
+
+# join the neighborhood info for pickup
+spdf <- as.data.frame(taxi_clean)
+coordinates(spdf) <- ~pickup_longitude +pickup_latitude
+proj4string(spdf) <- proj4string(nyc_neighborhoods)
+matches <- over(spdf, nyc_neighborhoods)
+taxi_clean <- cbind(taxi_clean, matches)
+
+
+# rename fields created for pickup neighborhoods
+taxi_clean <- rename(taxi_clean, pickup_neighborhood = neighborhood,pickup_borough=borough, pickup_boroughCode = boroughCode, pickup_X.id = X.id)
+
+#  join the neighborhood info for dropoff
+spdf <- as.data.frame(taxi_clean)
+coordinates(spdf) <- ~dropoff_longitude +dropoff_latitude
+proj4string(spdf) <- proj4string(nyc_neighborhoods)
+matches <- over(spdf, nyc_neighborhoods)
+taxi_clean <- cbind(taxi_clean, matches)
+
+# rename dropoff neighborhood field
+taxi_clean <- rename(taxi_clean, dropoff_neighborhood = neighborhood,dropoff_borough=borough, dropoff_boroughCode = boroughCode, dropoff_X.id = X.id)
+
+rm(spdf, nyc_df, min_lng, min_lat, max_lng, max_lat)
+
+
+
+save(taxi_clean, file = 'one_week_taxi.Rdata')
 
 
 
