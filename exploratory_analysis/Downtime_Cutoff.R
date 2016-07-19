c4adb41188f105e198be7e911835557192d642e3
Marieme Toure <touremarieme@yahoo.com>
2016-07-19 15:43:22 -0400

    Added the Downtime Cutoff

---
+++ exploratory_analysis/Downtime_Cutoff.R
@@ -0,0 +1,30 @@
+load("one_week_taxi.Rdata")
+library(ggplot2)
+library(scales)
+library(tidyr)
+library(dplyr)
+
+theme_set(theme_minimal())
+
+###########################################
+#examine shifts, active hours, and downtime
+###########################################
+shifts <- taxi_clean %>% group_by(hack_license) %>% 
+  arrange(pickup_datetime) %>% 
+  mutate(downtime = (lead(pickup_datetime) - dropoff_datetime)/3600) %>% 
+  select(pickup_datetime, dropoff_datetime, day_of_the_week, downtime)
+
+##########################################################
+#plotting the number of intervals aver the downtime period
+##########################################################
+ggplot(shifts) + geom_density(aes(as.numeric(downtime) ), na.rm = T) + 
+  scale_x_log10(label = comma, breaks = seq(1, 20, 2)) 
+#The cutoff time is 6 hours
+
+
+
+
+
+
+
+
