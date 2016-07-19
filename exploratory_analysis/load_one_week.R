fe67d5c121b59e8b5c7ca2c03183357fcffa0788
Jake Hofman <jhofman@gmail.com>
2016-07-18 20:02:12 -0400

    few speedups on datetime parsing

--- exploratory_analysis/load_one_week.R
+++ exploratory_analysis/load_one_week.R
@@ -6,33 +6,28 @@ library(httr)
 library(rgdal)
 library(broom)
 
-parse_datetime <- function(s, format="%Y-%m-%d %H:%M:%S") {
-  as.POSIXct(as.character(s), format=format)
-}
-
-parse_minute <- function(s, format = "%M"){
-  as.integer(strftime(s, format = format))
-  }
-
 #FILE DEPENDENCIES - TRIP_FARE_7 AND TRIP_DATA_7 CSVS
 
 # range of dates we're interested in
 range_begin <- as.Date("2013-07-07")
 range_end <- as.Date("2013-07-13")
 
-# read the files, add a ymd col and get the dates in ou range
+# read the files, add a ymd col and get the dates in our range
 trip_fare <- read_csv("trip_fare_7.csv")
-trip_fare <- trip_fare %>% mutate( ymd_pickup = as.Date(parse_datetime(pickup_datetime, "%Y-%m-%d"))) %>% 
-                           filter(as.Date(ymd_pickup) >= range_begin & as.Date(ymd_pickup) <= range_end)
+trip_fare <- trip_fare %>%
+  mutate( ymd_pickup = as.Date(pickup_datetime) ) %>% 
+  filter(ymd_pickup >= range_begin & ymd_pickup <= range_end)
+
 
 trip_data <- read_csv("trip_data_7.csv")
-trip_data <- trip_data %>% mutate( ymd_pickup = as.Date(parse_datetime(pickup_datetime, "%Y-%m-%d"))) %>% 
-                           filter(as.Date(ymd_pickup) >= range_begin & as.Date(ymd_pickup) <= range_end)
+trip_data <- trip_data %>%
+  mutate( ymd_pickup = as.Date(pickup_datetime) ) %>% 
+  filter(ymd_pickup >= range_begin & ymd_pickup <= range_end)
 
 # join the two dataframes 
 taxi <- inner_join(trip_data, trip_fare, by=c("medallion", "hack_license", "vendor_id", "pickup_datetime", "ymd_pickup"))
 taxi <- taxi %>% mutate(pickup_hour = hour(pickup_datetime), dropoff_hour = hour(dropoff_datetime),day_of_the_week = wday(ymd_pickup,label = TRUE), 
-                        pickup_minute = parse_minute(pickup_datetime), dropoff_minute = parse_minute(dropoff_datetime)) 
+                        pickup_minute = minute(pickup_datetime), minute = parse_minute(dropoff_datetime)) 
 
 get_nyc_neighborhoods <- function(){
   r <- GET('http://data.beta.nyc//dataset/0ff93d2d-90ba-457c-9f7e-39e47bf2ac5f/resource/35dd04fb-81b3-479b-a074-a27a37888ce7/download/d085e2f8d0b54d4590b1e7d1f35594c1pediacitiesnycneighborhoods.geojson')
@@ -74,8 +69,6 @@ taxi_clean <- cbind(taxi_clean, matches)
 taxi_clean <- rename(taxi_clean, dropoff_neighborhood = neighborhood,dropoff_borough=borough, dropoff_boroughCode = boroughCode, dropoff_X.id = X.id)
 
 rm(spdf, nyc_df, min_lng, min_lat, max_lng, max_lat)
-
-
 
 save(taxi_clean, file = 'one_week_taxi.Rdata')
 
