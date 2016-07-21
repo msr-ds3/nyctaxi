f6d1358c5171cdfa9459a7467831f478eff3e7d8
Abraham Neuwirth <abeneuwirth@gmail.com>
2016-07-21 16:58:54 -0400

    added get_map_by_driver function

--- exploratory_analysis/ggheatmaps.R
+++ exploratory_analysis/ggheatmaps.R
@@ -1,9 +1,14 @@
-source("one_week_analysis.R")
+load("shifts.R")
 library(ggmap)
 library(scales)
 library(maptools)
 library(gganimate)
 
+get_nyc_neighborhoods <- function(){
+  r <- GET('http://data.beta.nyc//dataset/0ff93d2d-90ba-457c-9f7e-39e47bf2ac5f/resource/35dd04fb-81b3-479b-a074-a27a37888ce7/download/d085e2f8d0b54d4590b1e7d1f35594c1pediacitiesnycneighborhoods.geojson')
+  return(readOGR(content(r,'text'), 'OGRGeoJSON', verbose = F))
+}
+nyc_neighborhoods <- get_nyc_neighborhoods()
 nyc_map <- get_map(location = c(lon = -73.95, lat = 40.71), maptype = "terrain", zoom = 11, color="bw")
 
   # takes a map, a dataframe, a spdf, and a vector of the form c("spdf_id"="df_id") 
@@ -12,7 +17,6 @@ make_ggmap <- function(df, map, spdf, join_by, boundary_color="black", boundary_
                        pal = "Spectral", transform = "log10", breaks =c(1 %o% 10^(0:5)), na_color = "#808080", title=waiver())
 {
   plot_data <- get_plot_df(spdf, df, join_by)
-  
   ggmap(map) + 
     geom_polygon(data=plot_data, aes(x=long, y=lat, group=group, fill=summary), 
                  color=boundary_color, size = boundary_thickness, alpha=fill_alpha) + 
@@ -31,5 +35,19 @@ get_map_by_range <- function(df, begin=0, end=24, group_by, filter_by ="pickup_h
     group_by_(group_by) %>%
     summarize(summary = n()/(7*(end-begin)))
   make_ggmap(data, nyc_map, nyc_neighborhoods, join_by = c("id"=group_by), title=begin)
+}
+
+get_map_by_driver <- function(df, group_by, driver, shift = NULL){
+  data <- df %>% filter(hack_license == driver)
+  if (!is.null(shift))
+  {
+    data <- data %>% filter(index == shift) 
+  }
+    data <- data %>% 
+      group_by_(group_by) %>%
+    summarize(summary = n())
+  make_ggmap(data, nyc_map, nyc_neighborhoods, join_by = c("id"=group_by), 
+             transform = "identity", 
+             breaks = seq(1, max(data$summary), max(data$summary)/10))
 }
 
