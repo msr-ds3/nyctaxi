33d591923d0585cf2e2b22f5873913d4aedab3d2
Jai Punjwani <jaipunjwani@mail.adelphi.edu>
2016-07-25 18:33:15 -0400

    Major cleaning up to speed up computations

--- prediction_task/shifts_design_matrix.R
+++ prediction_task/shifts_design_matrix.R
@@ -4,22 +4,37 @@ library(lubridate)
 library(dplyr)
 
 
-# set threshold for minimum num of rides per shift
-threshold <- round(mean(shifts_clean$total_trips) - sd(shifts_clean$total_trips))
+# set threshold for minimum length of shift
+threshold <- round(mean(shifts_clean$shift_length) - sd(shifts_clean$shift_length))
+thresholdMin = mean(shifts_clean$shift_length) - sd(shifts_clean$shift_length)
+thresholdMax = mean(shifts_clean$shift_length) + 2*sd(shifts_clean$shift_length)
 
 
 # find top n pickup neighborhoods
-n <- 20
+n_popular <- 20
+n_unpopular <- 50
 popular_pickup_neighborhoods <- taxi_clean_shifts %>%
   group_by(pickup_neighborhood) %>% 
   summarize(numtrips = n()) %>% 
-  top_n(n, numtrips)
+  top_n(n_popular, numtrips)
 
-# findtop n  dropoff neighborhoods
+# find top n  dropoff neighborhoods
 popular_dropoff_neighborhoods <- taxi_clean_shifts %>%
   group_by(dropoff_neighborhood) %>% 
   summarize(numtrips = n()) %>% 
-  top_n(n, numtrips)
+  top_n(n_popular, numtrips)
+
+# find bottom n  dropoff neighborhoods
+unpopular_pickup_neighborhoods <- taxi_clean_shifts %>%
+  group_by(pickup_neighborhood) %>% 
+  summarize(numtrips = n()) %>% 
+  top_n(n= n_unpopular, wt = -numtrips)
+
+unpopular_dropoff_neighborhoods <- taxi_clean_shifts %>%
+  group_by(dropoff_neighborhood) %>% 
+  summarize(numtrips = n()) %>% 
+  top_n(n = n_unpopular, wt= -numtrips)
+
 
 # returns 1 if this == that, otherwise 0
 is_equal_to <- function(this, that)
@@ -56,6 +71,38 @@ is_in_popular_pickup_neighborhoods = Vectorize(is_in_popular_pickup_neighborhood
 is_in_popular_dropoff_neighborhoods = Vectorize(is_in_popular_dropoff_neighborhoods)
 is_to_airport = Vectorize(is_to_airport)
 
+# Add "counter" columns to allow us to sum up easily without grouping
+taxi_clean_shifts = mutate(taxi_clean_shifts, 
+                           is_popular_pickup_neighborhood =
+                             ifelse(pickup_neighborhood %in% 
+                                      popular_pickup_neighborhoods$pickup_neighborhood, 1, 0),
+                           is_popular_dropoff_neighborhood = 
+                             ifelse(dropoff_neighborhood %in% 
+                                      popular_dropoff_neighborhoods$dropoff_neighborhood, 1, 0),
+                           is_unpopular_pickup_neighborhood =
+                             ifelse(pickup_neighborhood %in% 
+                                      popular_pickup_neighborhoods$pickup_neighborhood, 1, 0),
+                           is_unpopular_dropoff_neighborhood = 
+                             ifelse(dropoff_neighborhood %in% 
+                                      popular_dropoff_neighborhoods$dropoff_neighborhood, 1, 0),
+                           is_rate_code_1 = ifelse(rate_code == 1, 1, 0),
+                           is_rate_code_2 = ifelse(rate_code == 2, 1, 0),
+                           is_rate_code_3 = ifelse(rate_code == 3, 1, 0),
+                           is_rate_code_4 = ifelse(rate_code == 4, 1, 0),
+                           is_rate_code_5 = ifelse(rate_code == 5, 1, 0),
+                           is_rate_code_6 = ifelse(rate_code == 6, 1, 0),
+                           is_man_pickup = ifelse(pickup_boroughCode == 1, 1, 0),
+                           is_bronx_pickup = ifelse(pickup_boroughCode == 2, 1, 0),
+                           is_bklyn_pickup = ifelse(pickup_boroughCode == 3, 1, 0),
+                           is_queens_pickup = ifelse(pickup_boroughCode == 4, 1, 0),
+                           is_si_pickup = ifelse(pickup_boroughCode == 5, 1, 0),
+                           is_man_dropoff = ifelse(dropoff_boroughCode == 1, 1, 0),
+                           is_bronx_dropoff = ifelse(dropoff_boroughCode == 2, 1, 0),
+                           is_bklyn_dropoff = ifelse(dropoff_boroughCode == 3, 1, 0),
+                           is_queens_dropoff = ifelse(dropoff_boroughCode == 4, 1, 0),
+                           is_si_dropoff = ifelse(dropoff_boroughCode == 5, 1, 0)
+                          )
+
 shifts_design_matrix = taxi_clean_shifts %>% 
   group_by(hack_license, shift_num) %>%
   summarize(
@@ -70,38 +117,41 @@ shifts_design_matrix = taxi_clean_shifts %>%
     total_trip_time = sum(trip_time_in_secs)/3600,
     avg_trip_time = mean(trip_time_in_secs)/3600,
     sd_trip_time = sd(trip_time_in_secs)/3600,
-    rate_code_1_pct = sum(is_equal_to(rate_code, 1))/num_trips,
-    rate_code_2_pct = sum(is_equal_to(rate_code, 2))/num_trips,
-    rate_code_3_pct = sum(is_equal_to(rate_code, 3))/num_trips,
-    rate_code_4_pct = sum(is_equal_to(rate_code, 4))/num_trips,
-    rate_code_5_pct = sum(is_equal_to(rate_code, 5))/num_trips,
-    rate_code_6_pct = sum(is_equal_to(rate_code, 6))/num_trips,
+    rate_code_1_pct = sum(is_rate_code_1)/num_trips,
+    rate_code_2_pct = sum(is_rate_code_2)/num_trips,
+    rate_code_3_pct = sum(is_rate_code_3)/num_trips,
+    rate_code_4_pct = sum(is_rate_code_4)/num_trips,
+    rate_code_5_pct = sum(is_rate_code_5)/num_trips,
+    rate_code_6_pct = sum(is_rate_code_6)/num_trips,
     avg_speed = total_trip_distance/total_trip_time,
     start_day = wday(as.POSIXct(start,
                                 tz = "EDT", 
                                 origin = origin),
                      label = T),
     occupancy_pct = total_trip_time/length,
-    pickups_in_man_pct = sum(is_equal_to(pickup_boroughCode, 1))/num_trips,
-    pickups_in_bronx_pct = sum(is_equal_to(pickup_boroughCode, 2))/num_trips,
-    pickups_in_bklyn_pct = sum(is_equal_to(pickup_boroughCode, 3))/num_trips,
-    pickups_in_queens_pct = sum(is_equal_to(pickup_boroughCode, 4))/num_trips,
-    pickups_in_si_pct = sum(is_equal_to(pickup_boroughCode, 5))/num_trips,
-    dropoffs_in_man_pct = sum(is_equal_to(dropoff_boroughCode, 1))/num_trips,
-    dropoffs_in_bronx_pct = sum(is_equal_to(dropoff_boroughCode, 2))/num_trips,
-    dropoffs_in_bklyn_pct = sum(is_equal_to(dropoff_boroughCode, 3))/num_trips,
-    dropoffs_in_queens_pct = sum(is_equal_to(dropoff_boroughCode, 4))/num_trips,
-    dropoffs_in_si_pct = sum(is_equal_to(dropoff_boroughCode, 5))/num_trips,
+    pickups_in_man_pct = sum(is_man_pickup)/num_trips,
+    pickups_in_bronx_pct = sum(is_bronx_pickup)/num_trips,
+    pickups_in_bklyn_pct = sum(is_bklyn_pickup)/num_trips,
+    pickups_in_queens_pct = sum(is_queens_pickup)/num_trips,
+    pickups_in_si_pct = sum(is_si_pickup)/num_trips,
+    dropoffs_in_man_pct = sum(is_man_dropoff)/num_trips,
+    dropoffs_in_bronx_pct = sum(is_bronx_dropoff)/num_trips,
+    dropoffs_in_bklyn_pct = sum(is_bklyn_dropoff)/num_trips,
+    dropoffs_in_queens_pct = sum(is_queens_dropoff)/num_trips,
+    dropoffs_in_si_pct = sum(is_si_dropoff)/num_trips,
     popular_pickup_neighborhood_pct = 
-      sum(is_in_popular_pickup_neighborhoods(pickup_neighborhood))/num_trips,
+      sum(is_popular_pickup_neighborhood)/num_trips,
     popular_dropoff_neighborhood_pct = 
-      sum(is_in_popular_dropoff_neighborhoods(dropoff_neighborhood))/num_trips,
+      sum(is_popular_dropoff_neighborhood)/num_trips,
+    unpopular_pickup_neighborhood_pct = 
+      sum(is_unpopular_pickup_neighborhood)/num_trips,
+    unpopular_dropoff_neighborhood_pct = 
+      sum(is_unpopular_dropoff_neighborhood)/num_trips,
     airport_pct = sum(is_to_airport(dropoff_neighborhood, rate_code))/num_trips,
     efficiency = total_fare/length
   ) %>%
-  filter(num_trips >= threshold & 
-           length <= 24 &
-           #efficiency <= 75 &
+  filter(length >= thresholdMin & 
+           length <= thresholdMax &
            start >= as.POSIXct("2013-07-07 06:00:00", tz = "EDT") & 
            end <= as.POSIXct("2013-07-13 18:00:00", tz = "EDT"))
 
