d161ca828c940538ae37fdf1e49af3a5971fa3b2
Jai Punjwani <jaipunjwani@mail.adelphi.edu>
2016-07-28 14:50:31 -0400

    fixed split of data, ran model, classified efficiency as 0 or 1

--- prediction_task/predictions.R
+++ prediction_task/predictions.R
@@ -6,6 +6,7 @@ library(glmnet)
 library(lubridate)
 library(broom)
 library(tidyr)
+library(caret)
 
 ############################################
 #Added a new column for efficiency_category
@@ -23,49 +24,83 @@ min_num_shifts = 15
 valid_drivers =
   shifts_design_matrix %>%
   group_by(hack_license) %>%
-  summarize(num_shifts = max(shift_num)) %>%
+  summarize(num_shifts = n()) %>% 
   filter(num_shifts >= min_num_shifts) %>%
   select(hack_license)
 # join data frames to only select shifts with drivers who meet threshold
 valid_shifts =
   left_join(valid_drivers, shifts_design_matrix)
 
-##############################################
-#Splitting the data into train and test frames
-##############################################
+
+###############################################
+#Splitting the data into train and test frames# 
+###############################################
+
+# make sure we only test on drivers we have trained on
+# solution 1 - join data frames with intersection of unique hack licenses
 set.seed(42)
 indexes <- sample(1:nrow(valid_shifts), size=0.2*nrow(valid_shifts))
-TTtest=valid_shifts[indexes, ]
-TTtrain=valid_shifts[-indexes, ]
-################################################
-#Predicting efficiency category
-################################################
-Model1 <- 
-  as.formula(as.factor(efficiency_category) ~ as.factor(hack_license) + 
-               is_week_end*start_hour) 
-X = sparse.model.matrix(Model1, TTtrain)
-Y = TTtrain$efficiency_category
-glm.model_1 <- glmnet(X, Y, family = "binomial", lambda = 0)
-plot(glm.model_1)
-coef(glm.model_1) %>% head()
+test=valid_shifts[indexes, ]
+train=valid_shifts[-indexes, ]
 
-plot_data <-glm.model_1 %>% 
+unique_hl_train = train %>%
+  select(hack_license) %>%
+  distinct()
+test = inner_join(unique_hl_train, test)
+
+unique_hl_test = test %>%
+  select(hack_license) %>%
+  distinct()
+train = inner_join(train, unique_hl_test)
+
+# solution 2 - group by driver, arrange by time, and split each driver's shifts into training/test 
+train = valid_shifts %>% 
+  group_by(hack_license) %>% 
+  arrange(start) %>% 
+  mutate(row_num = row_number()) %>% 
+  filter(row_num <= round(0.8*max(row_num)))
+
+test = anti_join(valid_shifts, train) 
+
+
+###############################################
+#Creating model to predict efficiency category#
+###############################################
+formula <- 
+  as.formula(as.factor(efficiency_category) ~ hack_license + 
+               is_week_end*start_hour) 
+X = sparse.model.matrix(formula, train)
+Y = train$efficiency_category
+glm_efficiency <- glmnet(X, Y, family = "binomial", lambda =0)
+
+
+# PREDICTION
+xtest = sparse.model.matrix(formula, test)
+test$predicted <- predict(glm_efficiency, newx = xtest, type = "response")
+
+# Assessing prediction - AUC, ROC, Accuracy
+RMSE <- sqrt(mean((test$efficiency_category-test$predicted)^2))
+
+# Setting threshold of 0.5 to classify predictions
+test <- test %>% 
+  mutate(efficiency_category_predicted = ifelse(predicted > 0.5, 1, 0))
+test_confusion_matrix = 
+  confusionMatrix(test$efficiency_category_predicted, test$efficiency_category)
+
+
+# separating `hack_license` label from its value and plotting distribution of coef
+plot_data <-glm_efficiency %>% 
   tidy() %>% 
   extract(term, c("hack_license","rest"), "(hack_license)(.*)") %>% 
   filter(hack_license == "hack_license") 
-ggplot(plot_data, aes(x = estimate)) + geom_histogram(binwidth = 0.1)  
- 
+
+ggplot(plot_data, aes(x = estimate)) + geom_histogram(binwidth = 0.1)
 ggsave("../figures/coef_distribution_of_hack_licenses.png")
 
-TTtest$predicted <- predict(glm.model_1, TTtest)
 
-RMSE <- sqrt(mean((TTtest$effiency_category-TTtest$predicted)^2))
-
-################################################################################
-
-#########################
-#Randomizing valid shifts
-#########################
+####################################################
+#Repeat modeling/predicting for shuffled data frame#
+####################################################
 
 hack_licenses_shuffled <-sample(valid_shifts$hack_license, nrow(valid_shifts))
 random_shifts = valid_shifts
@@ -74,25 +109,56 @@ random_shifts$hack_license = hack_licenses_shuffled
 ##############################################
 #Splitting the data into train and test frames
 ##############################################
+
+# make sure we only test on drivers we have trained on
+# solution 1 - join data frames with intersection of unique hack licenses
 set.seed(42)
 indexes <- sample(1:nrow(random_shifts), size=0.2*nrow(random_shifts))
-TTtest=random_shifts[indexes, ]
-TTtrain=random_shifts[-indexes, ]
+test=random_shifts[indexes, ]
+train=random_shifts[-indexes, ]
 
-################################################
-#Predicting efficiency using efficiency category
-################################################
-Model1 <- 
+unique_hl_train = train %>%
+  select(hack_license) %>%
+  distinct()
+test = inner_join(unique_hl_train, test)
+
+unique_hl_test = test %>%
+  select(hack_license) %>%
+  distinct()
+train = inner_join(train, unique_hl_test)
+
+# solution 2 - group by driver, arrange by time, and split each driver's shifts into training/test 
+train = random_shifts %>% 
+  group_by(hack_license) %>% 
+  arrange(start) %>% 
+  mutate(row_num = row_number()) %>% 
+  filter(row_num <= round(0.8*max(row_num)))
+
+test = anti_join(random_shifts, train) 
+
+###############################################
+#Creating model to predict efficiency category#
+###############################################
+formula <- 
   as.formula(as.factor(efficiency_category) ~ hack_license + 
                is_week_end*start_hour) 
-X = sparse.model.matrix(Model1, TTtrain)
-Y = TTtrain$efficiency_category
-glm.model_1 <- glmnet(X, Y, family = "binomial")
-plot(glm.model_1)
+X = sparse.model.matrix(formula, train)
+Y = train$efficiency_category
+glm_efficiency <- glmnet(X, Y, family = "binomial", lambda = 0)
 
 
-plot_data <-glm.model_1 %>% tidy() %>% 
+# Prediction
+xtest = sparse.model.matrix(formula, test)
+test$predicted <- predict(glm_efficiency, newx = xtest, type = "response")
+
+# Assessing prediction - AUC, ROC, Accuracy
+RMSE <- sqrt(mean((test$efficiency_category-test$predicted)^2))
+
+# separating `hack_license` label from its value and plotting distribution of coef
+plot_data <-glm_efficiency %>% tidy() %>% 
   extract(term, c("hack_license","rest"), "(hack_license)(.*)") %>% 
   filter(hack_license == "hack_license") 
-ggplot(plot_data, aes(x = estimate)) + geom_histogram(binwidth = 1)
+ggplot(plot_data, aes(x = estimate)) + geom_histogram(binwidth = 0.1)
 ggsave("../figures/coef_distribution_of_hack_licenses_shuffled.png")
+
+
