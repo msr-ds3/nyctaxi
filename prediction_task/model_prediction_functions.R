f1617963490a8edc6b0c781620b8e987c64de1f8
Jai Punjwani <jaipunjwani@mail.adelphi.edu>
2016-07-29 12:17:21 -0400

    added train/test and roc/auc functions

--- prediction_task/model_prediction_functions.R
+++ prediction_task/model_prediction_functions.R
@@ -17,5 +17,27 @@ get_test_data = function(df, train)
   
 }
 
+train_model_classification = function(train, formula)
+{
+formula = as.formula(formula)
+X = sparse.model.matrix(formula, train) 
+Y = train$efficiency_category
+glm_efficiency <- glmnet(X, Y, family = "binomial", lambda =0)
 
+}
+
+test_model_classification = function(formula, test, model)
+{
+  formula = as.formula(formula)
+  xtest = sparse.model.matrix(formula, test)
+  test$predicted <- predict(model, newx = xtest, type = "response")
+  test
+}
+
+plot_roc_auc = function(test)
+{
+  pred <- prediction(test$predicted, test$efficiency_category)
+  perf = performance(pred, measure = 'tpr', x.measure = 'fpr')
+  plot(perf, main = paste ("ROC CURVE\nAUC: ", performance(pred, 'auc')@y.values))
+}
 
